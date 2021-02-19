97013da1902bf148ac5bcf60b09794acf98750c6
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-19 14:32:07 +0100

    fixed config problems

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
 module.exports = {
-	VIEWER_PORT      : process.env.PORT,
-	BOT_USERNAME     : process.env.BOT_USERNAME || "mr. Bot",
-	BOT_PASSWORD     : process.env.BOT_PASSWORD,
-	SERVER           : process.env.BOT_PASSWORD || "localhost",
-	SERVER_PORT      : process.env.SERVER_PORT,
-	RECONNECT_DELAY  : process.env.RECONNECT_DELAY || 10,
-	MC_VERSION       : process.env.MC_VERSION || "1.16.5",
-	MOVE_DELAY_MAX   : process.env.MOVE_DELAY_MAX || 5,
-	MODE             : process.env.MODE || "goto",
+	VIEWER_PORT: process.env.PORT,
+	BOT_USERNAME: process.env.BOT_USERNAME || "mr. Bot",
+	BOT_PASSWORD: process.env.BOT_PASSWORD,
+	SERVER: process.env.SERVER || "localhost",
+	SERVER_PORT: process.env.SERVER_PORT,
+	RECONNECT_DELAY: process.env.RECONNECT_DELAY || 10,
+	MC_VERSION: process.env.MC_VERSION,
+	MOVE_DELAY_MAX: process.env.MOVE_DELAY_MAX || 5,
+	MODE: process.env.MODE || "goto",
 	GOTO_MAX_DISTANCE: process.env.GOTO_MAX_DISTANCE || 200,
 	GOTO_MIN_DISTANCE: process.env.GOTO_MIN_DISTANCE || -200,
 };
