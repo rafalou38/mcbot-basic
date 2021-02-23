18124e4514aa897b71e23660cc59088ea85a37de
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-23 15:34:46 +0100

    better initial config vars

--- src/config.js
+++ src/config.js
@@ -6,10 +6,10 @@ module.exports = {
 	BOT_PASSWORD: process.env.BOT_PASSWORD,
 	SERVER: process.env.SERVER || "localhost",
 	SERVER_PORT: process.env.SERVER_PORT,
-	RECONNECT_DELAY: process.env.RECONNECT_DELAY || 10,
+	RECONNECT_DELAY: process.env.RECONNECT_DELAY || 4,
 	MC_VERSION: process.env.MC_VERSION,
 	MOVE_DELAY_MAX: process.env.MOVE_DELAY_MAX || 5,
 	MODE: process.env.MODE || "goto",
-	GOTO_MAX_DISTANCE: process.env.GOTO_MAX_DISTANCE || 200,
-	GOTO_MIN_DISTANCE: process.env.GOTO_MIN_DISTANCE || -200,
+	GOTO_MAX_DISTANCE: process.env.GOTO_MAX_DISTANCE || 4000,
+	GOTO_MIN_DISTANCE: process.env.GOTO_MIN_DISTANCE || 1000,
 };
